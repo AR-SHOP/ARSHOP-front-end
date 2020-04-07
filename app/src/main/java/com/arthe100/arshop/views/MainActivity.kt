b79ec4a5d92cbb5ccd10fd06e4d4d54887538478
Hazhar Aziz <hazharaziz5555@gmail.com>
2020-04-07 19:43:35 +0430

    main activity

---
+++ app/src/main/java/com/arthe100/arshop/views/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.arthe100.arshop.views
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.arthe100.arshop.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_layout)
+    }
+}
