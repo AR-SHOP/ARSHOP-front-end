328af3b540433543d70129e0cf92c3021a7eb734
Hazhar Aziz <hazharaziz5555@gmail.com>
2020-04-08 17:53:54 +0430

    home fragment sample

---
+++ app/src/main/java/com/arthe100/arshop/views/adapters/HomeFragment.kt
@@ -0,0 +1,18 @@
+package com.arthe100.arshop.views.adapters
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+import com.arthe100.arshop.R
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.home_fragment_layout, container, false)
+    }
+}
