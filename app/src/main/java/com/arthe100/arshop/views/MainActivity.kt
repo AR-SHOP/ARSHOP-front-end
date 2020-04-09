768bfed5a5c509c3d12faa0779a0ae9c7d0c2ddd
Hazhar Aziz <hazharaziz5555@gmail.com>
2020-04-09 21:48:54 +0430

    search bar layout

--- app/src/main/java/com/arthe100/arshop/views/MainActivity.kt
+++ app/src/main/java/com/arthe100/arshop/views/MainActivity.kt
@@ -1,18 +1,22 @@
 package com.arthe100.arshop.views
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
+import androidx.appcompat.widget.Toolbar
 import com.arthe100.arshop.R
 import com.arthe100.arshop.views.adapters.BottomNavigationViewAdapter
-import com.arthe100.arshop.views.fragments.CartFragment
-import com.arthe100.arshop.views.fragments.CategoriesFragment
-import com.arthe100.arshop.views.fragments.HomeFragment
-import com.arthe100.arshop.views.fragments.ProfileFragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.miguelcatalan.materialsearchview.MaterialSearchView
+import com.miguelcatalan.materialsearchview.MaterialSearchView.SearchViewListener
 import kotlinx.android.synthetic.main.activity_main_layout.*
 
+
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var searchView: MaterialSearchView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,5 +24,43 @@ class MainActivity : AppCompatActivity() {
 
         var bottomNavBar = BottomNavigationViewAdapter(this, savedInstanceState)
                 .setBottomNavigationView()
+
+        var toolbar: Toolbar = tool_bar
+        setSupportActionBar(toolbar)
+        toolbar.setTitleTextColor(Color.RED)
+
+        searchView = MaterialSearchView(this)
+        searchView = search_view
+        searchView.setCursorDrawable(R.drawable.cursor)
+
+        searchView.setOnQueryTextListener(object : MaterialSearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                //Do some magic
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                //Do some magic
+                return false
+            }
+        })
+
+        searchView.setOnSearchViewListener(object : SearchViewListener {
+            override fun onSearchViewShown() {
+                //Do some magic
+            }
+
+            override fun onSearchViewClosed() {
+                //Do some magic
+            }
+        })
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu_layout, menu)
+        val item: MenuItem = menu.findItem(R.id.action_search)
+        searchView.setMenuItem(item)
+        return super.onCreateOptionsMenu(menu)
     }
 }
