e839d16424d2f045f25ff5a63f9cb0bcfd8c3ac1
Hazhar Aziz <hazharaziz5555@gmail.com>
2020-04-10 10:30:16 +0430

    searchable activity

---
+++ app/src/main/java/com/arthe100/arshop/views/SearchableActivity.kt
@@ -0,0 +1,11 @@
+package com.arthe100.arshop.views
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SearchableActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
