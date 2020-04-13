8f01cf7f8eac50f08aee94965ca14c6f12547a76
HosseinMehrmohammadi <Hossein.m.1459@gmail.com>
2020-04-13 07:51:57 +0430

    Downloader class has been developed.

--- app/src/main/java/com/arthe100/arshop/scripts/network/DownloadManager/Downloader.java
+++ app/src/main/java/com/arthe100/arshop/scripts/network/DownloadManager/Downloader.java
@@ -13,6 +13,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.arthe100.arshop.scripts.network.Service.FileDownloadClient;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
@@ -20,62 +22,24 @@ import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 
-public class Downloader {
-    private static final int PERMISSION_STORAGE_CODE = 1000;
-    EditText UrlEnt;
-    Button downloadBtn;
+public class Downloader extends AppCompatActivity
+{
 
-    /*@Override
+    private static final int PERMISSION_STORAGE_CODE = 1000;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        UrlEnt.findViewById(R.id.UrlEntry);
-        downloadBtn.findViewById(R.id.DownloadButton);
-
-        downloadBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-                    if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
-                        String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
-                        requestPermissions(permissions, PERMISSION_STORAGE_CODE);
-                    }
-                    else {
-                        startDownloading();
-                    }
-                }
-                else {
-                    startDownloading();
-                }
-            }
-        });
-    }*/
-
-    private void startDownloading() {
-        String url = UrlEnt.getText().toString().trim();
-        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
-
-        request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI | DownloadManager.Request.NETWORK_MOBILE);
-        request.setTitle("Download");
-        request.setDescription("Downloading file...");
-
-        request.allowScanningByMediaScanner();
-        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "" + System.currentTimeMillis());
-
-        //downloadManager downloadManager = (DownloadManager)getSystemService(Context.DOWNLOAD_SERVICE);
-        //downloadManager.enqueue(request);
     }
 
-    //@Override
+    @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch (requestCode) {
             case PERMISSION_STORAGE_CODE: {
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    startDownloading();
+                    downloadFile();
                 } else {
-                    //Toast.makeText(this, "Permission denied!!!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, "Permission denied!!!", Toast.LENGTH_SHORT).show();
                 }
             }
         }
