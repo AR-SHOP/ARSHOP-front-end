28b134a2167127f57a2451fa316fe389ca6bda4a
Hazhar Aziz <hazharaziz5555@gmail.com>
2020-04-24 21:57:21 +0430

    implement password visibility toggle

--- app/src/main/java/com/arthe100/arshop/views/fragments/LoginFragment.kt
+++ app/src/main/java/com/arthe100/arshop/views/fragments/LoginFragment.kt
@@ -1,33 +1,53 @@
 package com.arthe100.arshop.views.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.InputType
+import android.text.method.PasswordTransformationMethod
+import android.text.method.SingleLineTransformationMethod
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.arthe100.arshop.R
 import com.arthe100.arshop.views.ILoadFragment
 import kotlinx.android.synthetic.main.login_fragment_layout.*
+import org.w3c.dom.Text
+import kotlin.math.log
+
 
 class LoginFragment : Fragment(), ILoadFragment {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        val view =  inflater.inflate(R.layout.login_fragment_layout, container, false)
-
-        if (view != null) {
-        }
-
-        return view
+        return inflater.inflate(R.layout.login_fragment_layout, container, false)
     }
 
     override fun onStart() {
+        super.onStart()
+
+
         new_acc_link.setOnClickListener{
             loadFragment(SignUpFragment())
         }
-        super.onStart()
+
+        var passwordVisible = false
+
+        visibility_icon.setOnClickListener {
+            if (passwordVisible) {
+                login_password.transformationMethod = PasswordTransformationMethod.getInstance()
+                passwordVisible = false
+                visibility_icon.setColorFilter(resources.getColor(R.color.colorHint, null))
+            }
+            else {
+                login_password.transformationMethod = SingleLineTransformationMethod.getInstance()
+                passwordVisible = true
+                visibility_icon.setColorFilter(resources.getColor(R.color.colorPrimary, null))
+            }
+        }
     }
 
     override fun loadFragment(fragment: Fragment) {
